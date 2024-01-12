14cf506db6363181238e9f0e1e2356c30a637e13
Jamila <jamila19ravshanova@gmail.com>
2024-01-12 17:47:08 +0300

    work

--- main.js
+++ main.js
@@ -1,13 +1,34 @@
 //1-masala: Kvadratning perimetrini aniqlash
-let a = 5
-let p = 4 * a
-console.log("Kvadratning perimetri:", p);
+// let a = 5
+// let p = 4 * a
+// console.log("Kvadratning perimetri:", p);
 
 //2-masala: Kvadratning yuzasi
-let s = 5
-s = a**2
-console.log("Kvadratning yuzasi:", s);
+// let s = 5
+// s = a**2
+// console.log("Kvadratning yuzasi:", s);
 
 //3-masala: Kvadratning yuzasi va perimetri
+//let a = 5
+//let b = 6
+//let s = a * b
+//let p = 2 * (a+b)
+//console.log("Kvadratning yuzasi:", s);
+//console.log("Kvadratning perimetri:", p);
+
+
+//4-masala: Aylananing uzunligi
+//let p = 3.14
+//let d = 5
+//let l = p*d
+//console.log("Aylananing uzunligi", l);
+
+
+//5-masala: Kubning hajmi va sirti
+//let a = 5
+//let v = a**3
+//let s = 6*a**2
+//console.log("Kubning hajmi:",v);
+//console.log("kuning sirti:", s);
 
 
