c5fea2ab2657e4dee7bb2be806aa4560a47efefd
Jamila <jamila19ravshanova@gmail.com>
2024-01-12 17:33:27 +0300

    homework

---
+++ main.js
@@ -0,0 +1,13 @@
+//1-masala: Kvadratning perimetrini aniqlash
+let a = 5
+let p = 4 * a
+console.log("Kvadratning perimetri:", p);
+
+//2-masala: Kvadratning yuzasi
+let s = 5
+s = a**2
+console.log("Kvadratning yuzasi:", s);
+
+//3-masala: Kvadratning yuzasi va perimetri
+
+
